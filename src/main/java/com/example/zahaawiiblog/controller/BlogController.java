16f1ef22410cdd43b7dc8974002e64fd168406e0
Zahaawii <zahaawii@gmail.com>
2025-09-19 22:52:16 +0200

    minor changes to controller blog

--- src/main/java/com/example/zahaawiiblog/controller/BlogController.java
+++ src/main/java/com/example/zahaawiiblog/controller/BlogController.java
@@ -33,8 +33,8 @@ public class BlogController {
     }
 
     @GetMapping("/getbyid/{id}")
-    public Optional<Blog> getBlogByBlogId(@PathVariable Long id) {
-        return new ResponseEntity<>(blogService.findById(id), HttpStatus.OK).getBody();
+    public ResponseEntity<Optional<Blog>> getBlogByBlogId(@PathVariable Long id) {
+        return new ResponseEntity<>(blogService.findById(id), HttpStatus.OK);
     }
 
     @GetMapping("/getbyusername/{username}")
